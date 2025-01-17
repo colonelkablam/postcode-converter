d984e78b1e59a5fe24fe3a6365dd788224ded44b
Nick Harding <100789789+colonelkablam@users.noreply.github.com>
2025-01-17 15:22:39 +0000

    add basic postcode data look up functionality

---
+++ index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import axios from "axios";
+
+// constants
+const app = express();
+const port = 3000;
+const baseURL = "https://api.postcodes.io/postcodes";
+
+
+// MIDDLEWARE
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+
+// ROUTE HANDLING
+// landing page
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+})
+
+// single postcode submission route
+app.post("/get-single-info", async (req, res) => {
+try {
+    const response = await axios.get(`${baseURL}/${req.body.postcode}`)
+    res.render("index.ejs", {
+        locationData: response.data.result, 
+    }
+    );
+} catch (error) {
+    res.render("index.ejs", {
+        error: error.message, 
+    });
+}
+})
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}.`);
+})
